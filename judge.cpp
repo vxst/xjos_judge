c1562515d83749581af072036f3ed98a7120c5c9
Shan Ting <vxst@vxst.org>
2013-07-13 16:24:17 +0800

    Judge

--- judge.cpp
+++ judge.cpp
@@ -15,9 +15,11 @@
    status System Error:2048
    status Touched:4096
 */
+//TODO: Transation instead of Lock
 #include<cstdio>
 #include<string>
 #include<cstdlib>
+#include<cctype>
 #include<iostream>
 #include<unistd.h>
 #include<cstring>
@@ -33,20 +35,48 @@
 
 #define DB_ERROR 2
 #define NOT_FOUND 4
+#define SOME_ERROR 32768
 
 using namespace mysqlpp;
 using namespace std;
 
-int lang,sid,pid,grade=0;
+int lang,sid,pid,grade=0,spjid;
 char src[256];
 Connection conn,conn2,conn3,conn4;
-char execname[256],inname[256],outname[256],tmpname[256],stdname[256],statusname[256],resname[256];
-char randstrtable[]="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789";
+char execname[256],inname[256],outname[256],tmpname[256],stdname[256],statusname[256],resname[256],spjname[256];
 vector<string> allnames;
+
+struct tst_result{
+	int points,full_points;
+	char* message;
+};
+
+bool spj_isspj;
+char spj_spjtype[256],*spj_src;
+char spj_path[256];
+
 
 void writesocket(const char* socketserver,int port,const char* msg);
 void alertnodetoupdate(int,int,double,double,int);
 int isfileexist(const char*);
+
+//------------------------------SQL And So..-----------------------------
+
+int sqlconnect(){
+	conn.connect("xjos","210.33.7.109","judge","T5SD5NCnu2zGhYwu",3306);
+	if(!conn.connected())
+		return DB_ERROR;
+	conn2.connect("xjos","210.33.7.109","judge","T5SD5NCnu2zGhYwu",3306);
+	if(!conn2.connected())
+		return DB_ERROR;
+	conn3.connect("xjos","210.33.7.109","judge","T5SD5NCnu2zGhYwu",3306);
+	if(!conn3.connected())
+		return DB_ERROR;
+	conn4.connect("xjos","210.33.7.109","judge","T5SD5NCnu2zGhYwu",3306);
+	if(!conn4.connected())
+		return DB_ERROR;
+	return 0;
+}
 
 void mkset(){
 	Query q=conn4.query();
@@ -80,13 +110,41 @@ void addstatus(int a){
 	qout<<"UPDATE `xjos`.`submit` SET status="<<status<<" WHERE sid="<<sid;
 	qout.use();
 }
+int closesqls(){
+	conn.disconnect();
+	conn2.disconnect();
+	conn3.disconnect();
+	conn4.disconnect();
+	return 0;
+}
+
+//--------------------------FS and names--------------------------------
+
+char randstrtable[]="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789";
+char randstr_rands[]="0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+char* randstr(int len){
+	char* str=(char*)calloc(len+1,sizeof(char));
+	for(int i=0,sl=strlen(randstr_rands);i<len;i++)
+		str[i]=randstr_rands[rand()%sl];
+	str[len]=0;
+	return str;
+}
 
 void mkrandstr(char* str,int len){
 	assert(len>0);
+	str[len]=0;
 	while(len--)
 		str[len]=randstrtable[rand()%strlen(randstrtable)];
 }
 
+void mkrandnameendwith(char* name,const char* end){
+	mkrandstr(tmpname,8);
+	name[0]=0;
+	strcpy(name,"/home/judge/");
+	strcat(name,tmpname);
+	strcat(name,end);
+	allnames.push_back(string(name));
+}
 void mkrandname(char* name){
 	mkrandstr(tmpname,8);
 	strcpy(name,"/home/judge/");
@@ -106,25 +164,34 @@ void setrandname(int isexec){
 		system(order);
 	}else{
 		mkrandname(execname);
+		usleep(100000);
 		sprintf(order,"mv /home/judge/test_exec %s",execname);
 		system(order);
+//		usleep(100000);
+		sprintf(order,"chmod +x %s",execname);
+		system(order);
+//		usleep(100000);
 	}
 }
-int sqlconnect(){
-	conn.connect("xjos","210.33.7.109","judge","T5SD5NCnu2zGhYwu",3306);
-	if(!conn.connected())
-		return DB_ERROR;
-	conn2.connect("xjos","210.33.7.109","judge","T5SD5NCnu2zGhYwu",3306);
-	if(!conn2.connected())
-		return DB_ERROR;
-	conn3.connect("xjos","210.33.7.109","judge","T5SD5NCnu2zGhYwu",3306);
-	if(!conn3.connected())
-		return DB_ERROR;
-	conn4.connect("xjos","210.33.7.109","judge","T5SD5NCnu2zGhYwu",3306);
-	if(!conn4.connected())
-		return DB_ERROR;
+int deletefile(const char* name){
+	char* str=new char[256];
+	sprintf(str,"rm %s",name);
+	system(str);
+	delete[] str;
 	return 0;
 }
+int deletefiles(){
+/*	deletefile(inname);
+	deletefile(outname);
+	deletefile(stdname);
+	deletefile(statusname);
+	deletefile(execname);*/
+	for(int i=0;i<allnames.size();i++){
+		deletefile(allnames[i].c_str());
+	}
+	return 0;
+}
+//--------------------------------Compile-----------------------------
 char* getmessage(int isok){
 	FILE * pFile;
 	long lSize;
@@ -150,53 +217,6 @@ char* getmessage(int isok){
 	fclose (pFile);
 	return buffer;
 }
-int fetch(){
-	char order[512];
-	Query qout=conn.query();
-	
-	qout<<"LOCK TABLES `xjos`.`submit` WRITE";
-	if(!qout.exec())
-		fprintf(stderr,"!!!SQLERROR!!!");
-
-	qout<<"SELECT pid,sid,language FROM `submit` WHERE `status`=0 LIMIT 0,1";
-	Row r=qout.use().fetch_row();
-	if(!r){
-		qout<<"UNLOCK TABLES";
-		if(!qout.exec())
-			fprintf(stderr,"!!!SQLERROR!!!");
-		return NOT_FOUND;
-	}
-	//cerr<<"Fetched!!"<<r["source"]<<endl;
-	lang=r["language"];
-	sid=r["sid"];
-	pid=r["pid"];
-
-	fprintf(stderr,"Starting SID:%d\n",sid);
-	
-	qout<<"UPDATE `submit` SET `status`="<<4096<<" WHERE sid="<<sid;
-	if(!qout.exec())
-		fprintf(stderr,"!!!SQLERROR!!!");
-
-	qout<<"UNLOCK TABLES";
-	if(!qout.exec())
-		fprintf(stderr,"!!!SQLERROR!!!");
-	fprintf(stderr,"Started SID:%d\n",sid);
-	ofstream fout("test");
-
-	qout<<"SELECT source FROM `submit` WHERE sid="<<sid;
-	r=qout.use().fetch_row();
-	fout<<r["source"]<<endl;
-
-	return 0;
-}
-char randstr_rands[]="0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
-char* randstr(int len){
-	char* str=(char*)calloc(len+1,sizeof(char));
-	for(int i=0,sl=strlen(randstr_rands);i<len;i++)
-		str[i]=randstr_rands[rand()%sl];
-	str[len]=0;
-	return str;
-}
 //ret:1 unknown; 2 cok 4 ce 8 noneed 16 SE
 int compile(){
 	char order[4096];
@@ -215,7 +235,7 @@ int compile(){
 		freopen("ci","w",stderr);
 		sprintf(order,r["compiler"].c_str(),src,"test_exec","ci");
 		system(order);
-		usleep(1000000);//Should use proc check to produce a better solution
+		usleep(750000);//Should use proc check to produce a better solution
 		fclose(stderr);
 		int isok=isfileexist("test_exec");
 
@@ -261,10 +281,119 @@ int compile(){
 		sprintf(order,"mv %s /home/judge/test_exec",src);
 		system(order);
 		system("chown judge:judge /home/judge/test_exec");
+		system("chmod 777 /home/judge/test_exec");
 	}
 	decstatus(1);
 	setrandname(1);
 	return retv;
+}
+int compilespj(int spjid,const char* tmppath,const char* savepath,Connection& conn){//tmppath must be .cpp
+	char order[4096];
+	Query sqlc=conn.query();
+	sqlc<<"SELECT spjsrc,spjbin,spjtype,spjbin_iscompiled FROM xjos.spj_table WHERE spjid="<<spjid;
+	//Currently spjbin has no use at all
+	Row r=sqlc.use().fetch_row();
+	spj_src=(char*)calloc(strlen(r["spjsrc"].c_str())+32,sizeof(char));
+	strcpy(spj_src,r["spjsrc"].c_str());
+	FILE* spjsrcf=fopen(tmppath,"w");
+	fputs(spj_src,spjsrcf);
+	fflush(spjsrcf);
+	sprintf(order,"g++ %s -o %s -O2",tmppath,savepath);
+	system(order);
+	usleep(500000);
+	if(!isfileexist(savepath)){
+		return 0;
+	}else{
+		return 1;
+	}
+}
+
+//-----------------------------------------Fetch----------------------------------------
+
+int fetch(){
+	char order[512];
+	Query qout=conn.query();
+	
+	qout<<"LOCK TABLES `xjos`.`submit` WRITE,`xjos`.`problem` WRITE";
+	if(!qout.exec())
+		fprintf(stderr,"!!!SQLERROR!!!");
+
+	qout<<"SELECT problem.pid,submit.sid,submit.language,problem.spjid FROM `submit` JOIN `problem` ON problem.pid=submit.pid WHERE `status`=0 LIMIT 0,1";
+	Row r=qout.use().fetch_row();
+	if(!r){
+		qout<<"UNLOCK TABLES";
+		if(!qout.exec())
+			fprintf(stderr,"!!!SQLERROR!!!");
+		return NOT_FOUND;
+	}
+	//cerr<<"Fetched!!"<<r["source"]<<endl;
+	lang=r["language"];
+	sid=r["sid"];
+	pid=r["pid"];
+	spjid=r["spjid"];
+
+	fprintf(stderr,"Starting SID:%d\n",sid);
+	
+	qout<<"UPDATE `submit` SET `status`="<<4096<<" WHERE sid="<<sid;
+	if(!qout.exec())
+		fprintf(stderr,"!!!SQLERROR!!!");
+
+	qout<<"UNLOCK TABLES";
+	if(!qout.exec())
+		fprintf(stderr,"!!!SQLERROR!!!");
+	fprintf(stderr,"Started SID:%d\n",sid);
+	ofstream fout("test");
+
+	qout<<"SELECT source FROM `submit` WHERE sid="<<sid;
+	r=qout.use().fetch_row();
+	fout<<r["source"]<<endl;
+
+	return 0;
+}
+
+//------------------------------------------Run------------------------------------
+//------------------------------------------Run Part--------------------------------
+int runspj(const char* inputfn,const char* outputfn,const char* stdfn,const char* spjfn,tst_result* result){
+	char* line=(char*)calloc(4096,sizeof(char)),*statusfn=(char*)calloc(256,sizeof(char)),order[4096];
+	result->message=(char*)calloc(65536,sizeof(char));
+	mkrandname(statusfn);
+	sprintf(order,"%s %s %s %s %s",spjfn,inputfn,stdfn,outputfn,statusfn);
+	int r=system(order);
+	if(r!=0)return SOME_ERROR;
+
+	usleep(500000);
+	FILE* statusfp=fopen(statusfn,"r");
+
+	int isinformation=0;
+	for(;!feof(statusfp);){
+		fgets(line,65536,statusfp);
+		if(!isinformation){
+			char type;
+			int i,maxscore;
+			for(i=0;line[i]&&line[i]!=':';i++);
+			if(!line[i]){
+				return SOME_ERROR;
+			}else{
+				type=tolower(line[0]);
+				char* strtmp=(char*)calloc(4096,sizeof(char));
+				strcpy(strtmp,line+i+1);
+				strcpy(line,strtmp);
+				free((void*)strtmp);
+			}
+			if(type=='i'){
+				isinformation=1;
+				strcat(result->message,line);
+				strcat(result->message,"\n");
+			}else if(type=='s'){
+				sscanf(line,"%d",&result->points);
+			}else if(type=='m'){
+				sscanf(line,"%d",&result->full_points);
+			}else{
+				return SOME_ERROR;
+			}
+		}
+	}
+	return 0;
 }
 int computetime(double a){
 	int ret=((int)a)+1;
@@ -285,16 +414,16 @@ run_result runonce(const char* execcommand,char* execname,char* inname,char* out
 	sprintf(exec_order,execcommand,execname);
 	sprintf(order,"su judge -c \"/usr/bin/time --quiet -o %s -f '%%U %%M' timeout --kill-after=1s %ds %s <%s >%s\"",statusname,computetime(timelimit),exec_order,inname,outname);
 	system(order);
+	//usleep(1000000);
 	FILE* stat=fopen(statusname,"r");
 	fscanf(stat,"%lf%lf",&ret.time,&ret.memory);
 	fclose(stat);
 	ret.memory/=4096;//To MB; Bug in GNU time make 4K Div
 	return ret;
 }
-struct tst_result{
-	int points,full_points;
-	char* message;
-};
+
+//------------------------------------------Test Part-----------------------------------
+
 tst_result testonce(char* tester,char* outname,char* stdname,char* resname,int points){
 	char order[256],*msg=new char[1024];
 	tst_result ret;
@@ -344,15 +473,15 @@ char* formatoutput(int point,run_result run,tst_result tst,double maxtime,double
 		strcpy(overallstatus,"WA");
 		laststatusid=32;
 	}
-	sprintf(ret,"<point><id>%d</id><mem>%.2lf</mem><time>%.2lf</time><status>%s</status><status_id>%d</status_id><score>%d</score></point>",point,run.memory,run.time,overallstatus,status_id,tst.points);
+	if(tst.message==NULL){
+		tst.message=(char*)calloc(10,sizeof(char));
+		strcpy(tst.message,"");
+	}
+	sprintf(ret,"<point><id>%d</id><mem>%.2lf</mem><time>%.2lf</time><status>%s</status><status_id>%d</status_id><score>%d</score><message>%s</message></point>",point,run.memory,run.time,overallstatus,status_id,tst.points,tst.message);
 
 	return ret;
 }
-void alertnodetoupdate(int dtid,int status,double mem,double time,int grade){
-	char* str=new char[256];
-	sprintf(str,"P%d:%d:%d:%.3lf:%.3lf:%d",sid,dtid,status,time,mem,grade);
-	writesocket("210.33.7.108",8127,str);
-}
+//----------------------------------------Main Run---------------------------
 int run(){
 	char runallstatus[65536];
 	*runallstatus=0;
@@ -382,7 +511,12 @@ int run(){
 		double ktime=r["problem_data_time"];
 		double mxmem=r["problem_data_memory"];
 		run_result rn=runonce(lang["runner"],execname,inname,outname,ktime,mxmem);
-		tst_result ts=testonce((char*)"tester",outname,stdname,resname,r["problem_data_score"]);
+		tst_result ts;
+		if(spjid==0){
+			ts=testonce((char*)"tester",outname,stdname,resname,r["problem_data_score"]);
+		}else{
+			runspj(inname,outname,stdname,spjname,&ts);
+		}
 		if(rn.time>ktime)
 			addstatus(256);
 		if(rn.memory>mxmem)
@@ -407,51 +541,9 @@ int run(){
 	decstatus(8);
 	return 0;
 }
-int print(){
-	fprintf(stderr,"Finished sid:%d",sid);
-	return 0;
-}
-int closesqls(){
-	conn.disconnect();
-	conn2.disconnect();
-	conn3.disconnect();
-	conn4.disconnect();
-	return 0;
-}
-int deletefile(const char* name){
-	char* str=new char[256];
-	sprintf(str,"rm %s",name);
-	system(str);
-	delete[] str;
-	return 0;
-}
-int deletefiles(){
-/*	deletefile(inname);
-	deletefile(outname);
-	deletefile(stdname);
-	deletefile(statusname);
-	deletefile(execname);*/
-	for(int i=0;i<allnames.size();i++){
-		deletefile(allnames[i].c_str());
-	}
-	return 0;
-}
-int main(){
-	srand(time(0));
-	sqlconnect();
-	if(fetch())return 0;
-	//addstatus(4096);//Touched
-//	mkset();
-	int cvv=compile();
-	if(cvv!=2&&cvv!=8)return 0;
-	run();
-	print();
-	closesqls();
-	deletefiles();
-}
 
+//--------------------------------Nodejs Communication--------------------------
 //Functions Of Network To STNODE
-
 void writesocket(const char* socketserver,int port,const char* msg)
 {
 	int sockfd, n;
@@ -483,3 +575,116 @@ int isfileexist(const char* fn){
 	fclose(f);
 	return 1;
 }
+
+void alertnodetoupdate(int dtid,int status,double mem,double time,int grade){
+	char* str=new char[256];
+	sprintf(str,"P%d:%d:%d:%.3lf:%.3lf:%d",sid,dtid,status,time,mem,grade);
+	writesocket("210.33.7.108",8127,str);
+}
+
+//----------------------------Something about TJDA Problem--------------------------
+int tjda_stid;
+int fetchtjdaprob(){
+	char order[512];
+	Query qout=conn.query();
+	
+	qout<<"LOCK TABLES `xjos`.`submit_tjda` WRITE,`xjos`.`problem_data` WRITE,`xjos`.`problem` WRITE";
+	if(!qout.exec())
+		fprintf(stderr,"!!!SQLERROR!!!");
+
+	qout<<"SELECT problem.spjid,submit_tjda.stid,submit_tjda.output,submit_tjda.rank,problem_data.problem_data_input,problem_data.problem_data_output,problem_data.problem_data_score FROM `submit_tjda` JOIN `problem_data` ON problem_data.pid=submit_tjda.pid AND problem_data.problem_data_rank=submit_tjda.rank JOIN problem ON problem.pid=submit_tjda.pid WHERE `isjudged`=0 LIMIT 0,1";
+
+	Row r=qout.use().fetch_row();
+	if(!r){
+		qout<<"UNLOCK TABLES";
+		if(!qout.exec())
+			fprintf(stderr,"!!!SQLERROR!!!");
+		return NOT_FOUND;
+	}
+	spjid=r["spjid"];
+	grade=r["problem_data_score"];
+	tjda_stid=r["stid"];
+
+	fprintf(stderr,"Starting STID:%d\n",tjda_stid);
+	
+	qout<<"UPDATE `submit_tjda` SET `isjudged`="<<1<<" WHERE stid="<<r["stid"];
+	if(!qout.exec())
+		fprintf(stderr,"!!!SQLERROR!!!");
+
+	qout<<"UNLOCK TABLES";
+	if(!qout.exec())
+		fprintf(stderr,"!!!SQLERROR!!!");
+	fprintf(stderr,"Started STID:%d\n",tjda_stid);
+	ofstream ftin("/home/judge/test.in");
+	ofstream ftout("/home/judge/test.out");
+	ofstream ftstd("/home/judge/test.std");
+
+	ftin<<r["problem_data_input"]<<endl;
+	ftstd<<r["problem_data_output"]<<endl;
+	ftout<<r["output"]<<endl;
+
+	return 0;
+}
+
+tst_result tjdajudgeonce(const char* tester,const char* outname,const char* stdname,const char* resname,int points){
+	char order[256],*msg=new char[1024];
+	tst_result ret;
+	ret.message=msg;
+	ret.full_points=points;
+	sprintf(order,"./tester %s %s %s %d",outname,stdname,resname,points);
+	system(order);
+	FILE* fres=fopen(resname,"r");
+	fscanf(fres,"%s%d",msg,&ret.points);
+	return ret;
+}
+void tjdaupdate(int stid,tst_result t){
+	Query qout=conn.query();
+	qout<<"UPDATE submit_tjda SET grade="<<t.points<<" WHERE stid="<<stid;
+	qout.exec();
+}
+
+
+
+//----------------------------And the main function---------------------------------
+
+int print(){
+	fprintf(stderr,"Finished sid:%d",sid);
+	return 0;
+}
+
+int main(){
+	char* anothertmpname=(char*)calloc(256,sizeof(char));;
+	srand(time(0));
+	sqlconnect();
+	if(fetch()){
+		if(!fetchtjdaprob()){
+			tst_result t;
+			if(spjid!=0){
+				char* spjsrc=(char*)calloc(256,sizeof(char));
+				mkrandnameendwith(spjsrc,".cpp");
+				char* spjrun=(char*)calloc(256,sizeof(char));
+				mkrandname(spjrun);
+				compilespj(spjid,spjsrc,spjrun,conn2);
+				runspj("/home/judge/test.in","/home/judge/test.out","/home/judge/test.std",spjrun,&t);
+			}else{
+				t=tjdajudgeonce("tester","test.out","test.std","test.res",grade);
+			}
+			tjdaupdate(tjda_stid,t);
+		}
+		return 0;
+	}
+	if(spjid!=0){
+		mkrandname(spjname);
+		mkrandnameendwith(anothertmpname,".cpp");
+		compilespj(spjid,anothertmpname,spjname,conn3);
+	}
+	//addstatus(4096);//Touched
+//	mkset();
+	int cvv=compile();
+	if(cvv!=2&&cvv!=8)return 0;
+	run();
+	print();
+	closesqls();
+	deletefiles();
+}
+
--- tester.cpp
+++ tester.cpp
@@ -5,7 +5,7 @@
 #include<cassert>
 //1:Out 2:Std 3:Res
 char* getnexttoken(FILE* file){
-	char* ret=new char[16777216];//FIXIT:Need to be unlimited if possible
+	char* ret=new char[16000000];//FIXIT:Need to be unlimited if possible
 	int k=fscanf(file,"%s",ret);
 	if(k!=1){
 		delete[] ret;
@@ -18,11 +18,14 @@ int main(int count, char** filename){
 	FILE* fout=fopen(filename[1],"r");
 	FILE* fstd=fopen(filename[2],"r");
 	FILE* fres=fopen(filename[3],"w");
+	FILE* fdbg=fopen("/home/judge/ss","a");
 	int point;
 	char* out,*std;
 	sscanf(filename[4],"%d",&point);
 	for(out=getnexttoken(fout),std=getnexttoken(fstd);out!=0&&std!=0;out=getnexttoken(fout),std=getnexttoken(fstd)){
+		fprintf(fdbg,"Compare:Std:%s:Out:%s\n",std,out);
 		if(strcmp(out,std)!=0){
+			fprintf(fdbg,"%s != %s\n",out,std);
 			fprintf(fres,"W\n");
 			fprintf(fres,"0\n");
 			fclose(fres);
@@ -32,6 +35,12 @@ int main(int count, char** filename){
 		delete[] std;
 	}
 	if((out==0&&std!=0)||(out!=0&&std==0)){
+		fprintf(fdbg,"Num Err\n");
+		if(out!=0){
+			fprintf(fdbg,"Out:%s\n",out);
+		}else{
+			fprintf(fdbg,"Std:%s\n",std);
+		}
 		fprintf(fres,"W\n0\n");
 	}else{
 		fprintf(fres,"A\n%d\n",point);
